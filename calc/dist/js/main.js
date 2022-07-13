6a26a9dcbbff7b830a8a4f29b5410ae35d47cc05
Diana Ivanova <diana.yastrebova02@mail.ru>
2022-07-13 11:20:53 +0300

    slider

--- calc/dist/js/main.js
+++ calc/dist/js/main.js
@@ -14,3 +14,17 @@ $(document).ready(function () {
     })
 })
 
+$(document).ready(function () {
+    $('.img__block-slider').slick({
+        dots: false,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 1,
+        arrows: true,
+        speed: 500,
+        useTransform: true,
+        cssEase: 'cubic-bezier(0.770, 0.000, 0.175, 1.000)',
+    });
+})
+
